239aed4ff6643fc24400987926232810ec83a59d
Wadson Vaval <vavalson@hotmail.com>
2021-11-30 17:35:33 -0400

    first commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// +------------------------------------------------------------------------+
+
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost:3306";
+// MySQL Database User
+$sql_db_user = "dlvpajmm_vwanu";
+// MySQL Database Password
+$sql_db_pass = "?6xs9wI0";
+// MySQL Database Name
+$sql_db_name = "dlvpajmm_vwanu";
+
+// Site URL
+$site_url = "https://www.vwanu.com"; // e.g (http://example.com)
+
+?>
---
+++ themes/wondertag/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'Wondertag';
+
+$themeFolder = 'wondertag';
+
+$themeAuthor = 'Kulvir Singh';
+
+$themeAuthorUrl = 'https://bit.ly/k_97';
+
+$themeVirsion = '2.3.4';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
